b3d2ba6bc9a50e522b923872cab292d678830edf
Maarten Paauw <maartenpaauw@gmail.com>
2016-02-07 16:29:05 +0100

    Opdracht 2

---
+++ Module/Week 1/src/Huiswerk/Dier.java
@@ -0,0 +1,26 @@
+package Huiswerk;
+
+import javafx.scene.paint.Color;
+
+public class Dier {
+
+	protected String naam;
+	protected Color kleur;
+	protected int poten;
+	protected int lengtePoten;
+	protected boolean staart;
+	protected String praat;
+
+	public Dier(String naam, Color kleur, int poten, int lengtePoten, boolean staart, String praat) {
+		this.naam = naam;
+		this.kleur = kleur;
+		this.poten = poten;
+		this.lengtePoten = lengtePoten;
+		this.staart = staart;
+		this.praat = praat;
+	}
+
+	public void praat() {
+		System.out.println(this.praat);
+	}
+}
---
+++ Module/Week 1/src/Huiswerk/Zoogdier.java
@@ -0,0 +1,17 @@
+package Huiswerk;
+
+import javafx.scene.paint.Color;
+
+public class Zoogdier extends Dier {
+
+	private String dier;
+
+	public Zoogdier(String naam, Color kleur, int poten, int lengtePoten, boolean staart, String praat, String dier) {
+		super(naam, kleur, poten, lengtePoten, staart, praat);
+		this.dier = dier;
+	}
+
+	public String toString() {
+		return "De " + this.dier + " " + this.naam + " heeft " + this.poten + " poten, die elk " + this.lengtePoten + " cm lang zijn en heeft " + ((this.staart) ? "wel" : "niet") + " een staart.";
+	}
+}
