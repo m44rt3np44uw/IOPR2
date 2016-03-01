6e3d5b66eab0533a96ef94836add109a0ac02904
Maarten Paauw <maartenpaauw@gmail.com>
2016-03-01 08:41:13 +0100

    Opdracht 9A

---
+++ Module/Week 3/src/Werkcollege/Opdracht9A.java
@@ -0,0 +1,20 @@
+package Werkcollege;
+
+public class Opdracht9A {
+
+	public static void main(String [] args) {
+
+		try {
+
+		}
+
+		catch(ArrayIndexOutOfBoundsException exception) {
+
+		}
+
+		finally {
+
+		}
+
+	}
+}
