725fc84001493fb3af8ce5d79ae2ebffe4698404
Maarten Paauw <maartenpaauw@gmail.com>
2016-03-01 13:53:46 +0100

    Opdracht 4C

---
+++ Module/Week 3/src/Huiswerk/Opdracht4C.java
@@ -0,0 +1,20 @@
+package Huiswerk;
+
+import java.util.ArrayList;
+
+public class Opdracht4C {
+
+	public static ArrayList<Pizza> allePizzas = new ArrayList<Pizza>();
+
+	public static void main(String[] args) {
+		allePizzas.add(new Bolognese());
+		allePizzas.add(new Calzone());
+		allePizzas.add(new QuattroStagionni());
+		allePizzas.add(new Tonno());
+
+		for (Pizza pizza : allePizzas) {
+			System.out.println("Pizza: " + pizza.getNaam() + " Kosten: €" + pizza.getPrijs());
+		}
+	}
+
+}
