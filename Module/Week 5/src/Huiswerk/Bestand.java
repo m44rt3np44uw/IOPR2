453051adb03bea21a2f71d74f16d0cd26f56ad0a
Maarten Paauw <maartenpaauw@gmail.com>
2016-03-13 14:57:07 +0100

    Opdracht 6

---
+++ Module/Week 5/src/Huiswerk/Bestand.java
@@ -0,0 +1,27 @@
+package Huiswerk;
+
+import java.io.File;
+import java.io.IOException;
+
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class Bestand extends Stage {
+
+	private FileChooser fileChooser = new FileChooser();
+	private String      bestand     = null;
+	private File        file;
+
+	public String openTekstBestand() {
+		 file = this.fileChooser.showOpenDialog(this);
+
+		try {
+			this.bestand = file.getCanonicalPath();
+		}
+
+		catch(IOException e) {}
+		catch (NullPointerException e) {}
+
+		return bestand;
+	}
+}
